0273fe3c9109e481d9c4f70eaaa3ec09ae72ba4a
granth1406 <grandygupta@gmail.com>
2025-01-30 12:42:23 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,22 +4,22 @@ int main() {
     char o;
     scanf("%lf %lf %c",&a,&b,&o);
     switch(o){
-            case '+':
-            printf("%.0lf",a+b);
-            break;
-            case '-':
-            printf("%.0lf",a-b);
-            break;
-            case '*':
-            printf("%.0lf",a*b);
-            break;
-            case '/':
-            if(b==0){
-                printf("error");
-            }else{
-                printf("%.0lf",a/b);
-            }
-            break;
+        case '+':
+        printf("%.0lf",a+b);
+        break;
+        case '-':
+        printf("%.0lf",a-b);
+        break;
+        case '*':
+        printf("%.0lf",a*b);
+        break;
+        case '/':
+        if(b==0){
+            printf("error");
+        }else{
+            printf("%.0lf",a/b);
+        }
+        break;
         
     }
     return 0;
