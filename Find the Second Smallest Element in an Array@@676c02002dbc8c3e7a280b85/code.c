a27b4d6ae9c16ee58fff0b731443956f791b9c9d
granth1406 <grandygupta@gmail.com>
2025-03-21 23:28:08 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
-
 int main() {
     int n;
-    scanf("%d",&n);
+
+    // Input size of the array
+    scanf("%d", &n);
+    
+    // If size is less than 2, second minimum does not exist
+    if (n < 2) {
+        printf("-1\n");
+        return 0;
+    }
+
     int arr[n];
 
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    int min=a[0];
-    int sec_min=a[1];
-    for(int i=1;i<n;i++){
-        if(arr[i]<min){
-            sec_min=min;
-            min=arr[i];
-        }else if(arr[i]<sec_min && arr[i]!=min){
-            sec_min=arr[i];
+
+    // Initialize minimum and second minimum
+    int min = arr[0], sec_min = __INT_MAX__; // Using maximum possible integer value
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) {
+            sec_min = min;
+            min = arr[i];
+        } else if (arr[i] < sec_min && arr[i] != min) {
+            sec_min = arr[i];
         }
     }
-    if(sec_min==-1){
-        printf("-1");
-    }else{
-        printf("%d",sec_min);
+
+    if (sec_min == __INT_MAX__) {
+        printf("-1\n");
+    } else {
+        printf("%d\n", sec_min);
     }
+
+    return 0;
 }
